90fba20fa033cb8e2b8468a96b25ac73da5e2fa3
Sani <pilyugaa4@gmail.com>
2022-08-08 16:25:48 +0300

    Fixed error with backtick

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,7 +4,7 @@ const NotFoundPage = () => {
     return (
         <div className='w-full h-full flex flex-col justify-center items-center'>
             <div className='w-[80%] text-center break-words text-xl font-semibold'>
-                Sup, we haven't implemented this idea yet but soon it comes true
+                Sup, we haven<>&apos;</>t implemented this idea yet but soon it comes true
                 🔥
             </div>
         </div>
